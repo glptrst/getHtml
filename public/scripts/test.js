fbe65de6efc1e9d10bc323a2d725787bff24b9c1
G P <giulio.pietroiusti@gmail.com>
2018-03-01 15:19:43 +0100

    Add event listener to submit button without affecting the get request.

--- public/scripts/test.js
+++ public/scripts/test.js
@@ -2,12 +2,8 @@ window.onload = function () {
     
     console.log('hello there');
 
-    // TODO: adding an event listener to the button makes the the get
-    // request fail. how to fix this?? I would like to insert a
-    // loading gif when the button is clicked.
-
-    // document.getElementById('loadingGif').addEventListener('click', () => {
-    // 	console.log('oi oi');
-    // });
+    document.getElementById('inputSpan').addEventListener('click', () => {
+    	console.log('loading');
+    });
 
 };
